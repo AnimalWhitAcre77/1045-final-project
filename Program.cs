a1cca1cf8b406a5ff158711f92c59a75abe9b3f6
unknown <AmmWhi48@millardk12.org>
2026-04-28 22:18:04 -0600

    finished the respawn/death sequence

--- Program.cs
+++ Program.cs
@@ -1,43 +1,53 @@
-﻿World world1 = new("World_1.txt");
-Player player = (Player)world1.Entities[0];
-Screen screen1 = new(world1, 30, 10, 0, 0);
+﻿World world;
+Player player;
+Screen screen;
 
-Console.Clear();
+LoadWorld("World_1.txt");
+
 Console.ForegroundColor = ConsoleColor.White;
 
+Console.WriteLine("Expand the terminal before starting.");
 Console.Write("Press any key to wake up... ");
 
 ConsoleKeyInfo input;
 do
 {
     input = Console.ReadKey(true);
-    world1.StepFrame(input);
+    world.StepFrame(input);
     
     // move the screen so the player is closer to center
-    screen1.Move((player.X - screen1.X - (screen1.Width / 2)) / (screen1.Width / 5), (player.Y - screen1.Y - (screen1.Height / 2)) / (screen1.Height / 5));
+    screen.Move((player.X - screen.X - (screen.Width / 2)) / (screen.Width / 5), (player.Y - screen.Y - (screen.Height / 2)) / (screen.Height / 5));
+    screen.Print(ConsoleColor.White);
 
     // Check if the player died
-    foreach (MovableEntity entity in world1.Entities[1..])
+    foreach (MovableEntity entity in world.Entities[1..])
     {
         if (player.DeadlyEntities.Contains(entity.GetType()))
         {
             if (entity.X == player.X && entity.Y == player.Y)
             {
-                screen1.Print(ConsoleColor.DarkGray);
+                screen.Print(ConsoleColor.DarkGray);
 
-                Console.SetCursorPosition(0, screen1.Height + 2);
+                Console.SetCursorPosition(0, screen.Height + 1);
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("You died!");
-                Console.Write("Press any key to wake up... ");
+                Console.Write("Press any key to wake up: ");
 
                 Console.ReadKey(true);
 
-                world1 = new World("World_1.txt");
+                LoadWorld("World_1.txt");
                 break;
             }
         }
     }
-
-    screen1.Print(ConsoleColor.White);
 }
 while (input.Key != ConsoleKey.Escape);
+
+void LoadWorld(string filePath)
+{
+    world = new World(filePath);
+    player = (Player)world.Entities[0];
+    screen = new(world, 30, 10, 0, 0);
+
+    Console.Clear();
+}
